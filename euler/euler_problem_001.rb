4d4b4af684e4f26379f3dd29addbcae43eb04b50
James <jamespittman@gmail.com>
2015-10-13 14:31:58 -0600

    rearranged loop variables and loop-dependent output statements into better organized blocks

--- euler/euler_problem_001.rb
+++ euler/euler_problem_001.rb
@@ -28,50 +28,45 @@
 #The final value is 233168
 #END OF LINE
 #
-
 # Ruby Module Dependencies (--max-depth = 1) | (alphabetized)
-# none needed
-
+#	none needed
 # Constants and variable initialization
 #
 first_number = 3
 second_number = 5
 loop_limit = 1000
 number_product = first_number * second_number
-
 # Runtime Execution Logic
 ## I had problems getting a single subroutine to work, reverting to bloated way
 ## using 3 separate loops; need to review variable passing to/from functions
-iterator_1 = 0
-iterator_2 = 0
-iterator_3 = 0
-decrementor_1 = first_number
-decrementor_2 = second_number
-decrementor_3 = number_product
-sum_of_multiple_1 = 0
-sum_of_multiple_2 = 0
-sum_of_multiple_3 = 0
 #loop 1
+iterator_1 = 0
+decrementor_1 = first_number
+sum_of_multiple_1 = 0
 while (iterator_1 < loop_limit - decrementor_1)
 	iterator_1 = iterator_1 + decrementor_1
 	sum_of_multiple_1 = sum_of_multiple_1 + iterator_1
 end
+puts "The value of sum_of_multiple for #{first_number} is #{sum_of_multiple_1}."
 #loop 2
+iterator_2 = 0
+decrementor_2 = second_number
+sum_of_multiple_2 = 0
 while (iterator_2 < loop_limit - decrementor_2)
 	iterator_2 = iterator_2 + decrementor_2
 	sum_of_multiple_2 = sum_of_multiple_2 + iterator_2
 end
+puts "The value of sum_of_multiple for #{second_number} is #{sum_of_multiple_2}."
 #loop 3
+iterator_3 = 0
+decrementor_3 = number_product
+sum_of_multiple_3 = 0
 while (iterator_3 < loop_limit - decrementor_3)
 	iterator_3 = iterator_3 + decrementor_3
 	sum_of_multiple_3 = sum_of_multiple_3 + iterator_3
 end
-
+puts "The value of sum_of_multiple for #{number_product} is #{sum_of_multiple_3}."
 # calculate sum of the first two outputs less that of their product
 final_value = sum_of_multiple_1 + sum_of_multiple_2 - sum_of_multiple_3
-# print output
-puts "The value of sum_of_multiple for #{first_number} is #{sum_of_multiple_1}."
-puts "The value of sum_of_multiple for #{second_number} is #{sum_of_multiple_2}."
-puts "The value of sum_of_multiple for #{number_product} is #{sum_of_multiple_3}."
 puts "The final value is #{final_value}."
 puts "END OF LINE"
